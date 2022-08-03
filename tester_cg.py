11cb9c64a8b2ee59913f93b694edba39b36c3c98
ithil0 <fernanda.rubio@sansano.usm.cl>
2022-08-03 11:08:07 -0400

    Publish project

--- tester_cg.py
+++ tester_cg.py
@@ -35,8 +35,7 @@ def load_texture(texture):
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
     glTexParameterf(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)  
-    
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)      
     return textura
 
 def tex():
@@ -61,57 +60,22 @@ class Cube:
         self.gamet=[0,1,2]
     
     def inicio(self):        
-        for i in range(3):
-            #glPushMatrix()
-            glEnable(GL_TEXTURE_2D) 
-            #glTranslatef(*[0, 0, 0])
+        for i in range(3):            
+            glEnable(GL_TEXTURE_2D)             
             glBindTexture(GL_TEXTURE_2D, self.texturas[self.gamet[i]])
-            glBegin(GL_QUADS)
-            #x=x+1
-            #glColor3fv(self.colors[x])            
+            glBegin(GL_QUADS)                       
             for j in range(4):
                 glTexCoord2f(self.coords[j][0],self.coords[j][1])
                 glVertex2f(self.vertexs[self.edges[i][j]][0],self.vertexs[self.edges[i][j]][1])      
           
-            glEnd()
-    
-    def dibujar(self):
-        self.keys=pg.key.get_pressed()        
-        a=random.choice(range(2,5))
-        d=random.choice(range(2,5))
-        if self.keys[K_a]:
-            glEnable(GL_TEXTURE_2D) 
-            glBindTexture(GL_TEXTURE_2D, self.texturas[a])
-            glBegin(GL_QUADS)
-            for j in range(4):
-                glTexCoord2f(self.coords[j][0],self.coords[j][1])
-                glVertex2f(self.vertexs[self.edges[1][j]][0],self.vertexs[self.edges[1][j]][1])
-            glEnd()
-        if self.keys[K_d]:
-            glEnable(GL_TEXTURE_2D) 
-            glBindTexture(GL_TEXTURE_2D, self.texturas[d])
-            glBegin(GL_QUADS)
-            for j in range(4):
-                glTexCoord2f(self.coords[j][0],self.coords[j][1])
-                glVertex2f(self.vertexs[self.edges[0][j]][0],self.vertexs[self.edges[0][j]][1])
-            glEnd()
-       
+            glEnd()      
        
     
-    def update(self):      
-        #self.keys=pg.key.get_pressed()  
-        #if self.keys[K_a]:
-         #   self.gamet[1]=random.choice(range(5,8))     
-        #self.event=pg.event.get()  
-        #if self.event.key==pg.K_a:
-        #    self.gamet[1]=random.choice(range(5,8))
-
-
+    def update(self): 
         self.mouse=pg.mouse.get_pressed()
         pos=pg.mouse.get_pos()
         x=(int(pos[0])-490)/60
-        y=(490-int(pos[1]))/60
-        #print(x,y)
+        y=(490-int(pos[1]))/60        
         for v1,v2 in self.vertexs:
             if abs(x-v1)<0.5 and abs(y-v2)<0.5 and self.mouse[0]:
                 index = self.vertexs.index((v1,v2))             
@@ -122,14 +86,11 @@ def main():
     pg.init()
     rockSound = pg.mixer.Sound("rock.wav")
     paperSound = pg.mixer.Sound("paper.wav")
-    tijSound = pg.mixer.Sound("scissors.wav")
-    #glMatrixMode(GL_PROJECTION)
+    tijSound = pg.mixer.Sound("scissors.wav")    
     display=(980,980)
     pg.display.set_mode(display,pg.DOUBLEBUF|pg.OPENGL)
-    gluPerspective(60,display[0]/display[1],1,150)
-    #glMatrixMode(GL_MODELVIEW)
-    glTranslatef(0,0.0,-15)
-    #glRotatef(45,0,1,0)
+    gluPerspective(60,display[0]/display[1],1,150)    
+    glTranslatef(0,0.0,-15)    
     objeto=Cube(vertex,aristas,colors,texturas)  
     programa=True  
     ini=True
@@ -177,12 +138,8 @@ def main():
                     programa=False           
         
         objeto.update()
-                
-
         glEnable( GL_DEPTH_TEST)
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        #if ini==True:
-           # objeto.inicio()
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)        
         objeto.inicio()
         pg.display.flip()
         pg.time.wait(10)
